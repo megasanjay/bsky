b295e35e52f27d624f6870f096cba2deb22df5e7
Sanjay Soundarajan <sanjay.16@live.com>
2024-12-03 16:33:59 -0800

    wip: get follows

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,48 @@
-import z from 'zod';
+import { AtpAgent } from '@atproto/api';
+import * as dotenv from 'dotenv';
 
-const isString = z.string();
+dotenv.config();
 
-function main() {
-  console.log(isString.parse('Hello TypeScript!'));
+// Check if the environment variables are set
+if (!process.env.BLUESKY_USERNAME || !process.env.BLUESKY_PASSWORD) {
+  console.error('BLUESKY_USERNAME and BLUESKY_PASSWORD must be set');
+  process.exit(1);
 }
+
+const main = async () => {
+  // Create a Bluesky Agent
+  const agent = new AtpAgent({
+    service: 'https://bsky.social',
+  });
+
+  // Login to Bluesky
+  try {
+    await agent.login({
+      identifier: process.env.BLUESKY_USERNAME as string,
+      password: process.env.BLUESKY_PASSWORD as string,
+    });
+  } catch (e) {
+    console.error('Login failed:', e);
+    process.exit(1);
+  }
+
+  console.log('Logged in as:', agent.assertDid);
+
+  // await agent.post({
+  //   text: '🙂',
+  // });
+
+  const follows = await agent.getFollows({
+    actor: agent.assertDid,
+  });
+
+  console.log('Your follows:', follows.data.follows);
+
+  const followers = await agent.getFollowers({
+    actor: agent.assertDid,
+  });
+
+  console.log('Your followers:', followers.data);
+};
 
 main();
